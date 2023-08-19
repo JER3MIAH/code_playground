fda3aef9efb31a8e51d797b4afc6fcc30afe84b0
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-19 19:02:14 +0100

    linked lists

--- linked_lists/singly_linked_lists/linked_list.c
+++ linked_lists/singly_linked_lists/linked_list.c
@@ -8,10 +8,11 @@ struct node
 	struct node *link;  /*pointer to struct node*/
 };
 
-int main()
+int main(void)
 {
 	struct node *head = NULL;  /*pointer to struct node(can access data and link)*/
     struct node *current = NULL;
+    struct node *current2 = NULL;
 
 	head = malloc(sizeof(struct node));  /*allocate memory for the node*/
 
@@ -23,6 +24,14 @@ int main()
     current->data = 45;
 	current->link = NULL;
     head->link = current; /*first node is now connected to second node*/
+
+    /*to create a third node...*/
+    current2 = malloc(sizeof(struct node));
+    current2->data = 3;
+	current2->link = NULL;
+    current->link = current2;
+
+    /*Though the above method isn't efficient as it wastes memory*/
 
 	return (0);
 }
