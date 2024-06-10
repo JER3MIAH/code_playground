68bc5511245637e1a0112ad6b9e5ee61f58fa1db
JER3MIAH <jeremiahbrownie@gmail.com>
2024-06-10 18:47:07 +0100

    Code clean up

--- Python/practice/rock_paper_scissors.py
+++ Python/practice/rock_paper_scissors.py
@@ -1,3 +1,5 @@
+"""Rock paper scissors game"""
+
 import random
 
 rand = random.randint(1, 5)
@@ -6,46 +8,58 @@ myComputerChoice = random.choice(game_options)
 
 
 def choice():
+    """This function formats and returns the user's choice
+    """
     try:
         ch = int(input('rock(1), paper(2), or scissors(3)? '))
         return ch
-    except ValueError as e:
+    except ValueError:
         return 0
 
 
-def setUserChoice(userInput):
-    if userInput == 1:
+def set_user_choice(user_input):
+    """This function selects rock, paper or scissors for the user
+    """
+    if user_input == 1:
         return 'rock'
-    elif userInput == 2:
+    elif user_input == 2:
         return 'paper'
     else:
         return 'scissors'
 
 
-def checkChoices(userChoice, computerChoice):
-    print(f'your pick: {userChoice}, computer\'s pick: {computerChoice}')
-    if userChoice == computerChoice:
+def check_choices(user_choice, computer_choice):
+    """This function checks user and computer choices to determine the winner
+    """
+    print(f'your pick: {user_choice}, computer\'s pick: {computer_choice}')
+    if user_choice == computer_choice:
         print('A tie!!')
-    elif (userChoice == game_options[0] and computerChoice == game_options[1]) or ((userChoice == game_options[1] and computerChoice == game_options[2])) or ((userChoice == game_options[2] and computerChoice == game_options[0])):
+    elif (user_choice == game_options[0] and computer_choice == game_options[1]) or\
+        ((user_choice == game_options[1] and computer_choice == game_options[2])) or\
+            ((user_choice == game_options[2] and computer_choice == game_options[0])):
         print('You lost!')
         return False
-    elif (userChoice == game_options[0] and computerChoice == game_options[2]) or ((userChoice == game_options[1] and computerChoice == game_options[0])) or ((userChoice == game_options[2] and computerChoice == game_options[1])):
+    elif (user_choice == game_options[0] and computer_choice == game_options[2]) or\
+        ((user_choice == game_options[1] and computer_choice == game_options[0])) or\
+            ((user_choice == game_options[2] and computer_choice == game_options[1])):
         print('You Win!')
         return True
 
 
-def startGame(userInput, computerChoice):
-    gameOn = True
+def start_game(user_input, computer_choice):
+    """This function starts the game
+    """
+    game_on = True
     print('Starting game...')
 
-    while gameOn:
-        if not isinstance(userInput, int) or userInput not in (1, 2, 3):
+    while game_on:
+        if not isinstance(user_input, int) or user_input not in (1, 2, 3):
             print('Enter 1, 2 or 3')
-            userInput = choice()
+            user_input = choice()
         else:
-            userChoice = setUserChoice(userInput)
-            checkChoices(userChoice, computerChoice)
-            gameOn = False
+            user_choice = set_user_choice(user_input)
+            check_choices(user_choice, computer_choice)
+            game_on = False
 
 
-startGame(choice(), myComputerChoice)
+start_game(choice(), myComputerChoice)
